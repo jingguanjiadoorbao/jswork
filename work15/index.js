dc735b28e2a98688ae11bc920d37e114af00fffb
jingguanjiadoorbao <55379127+jingguanjiadoorbao@users.noreply.github.com>
2020-01-01 16:38:43 +0800

    Add files via upload

---
+++ work15/index.js
@@ -0,0 +1,22 @@
+ShopCart('cart', [{
+    name: '只狼:影逝二度',
+    price:148.0,
+    num: 1
+},
+{
+    name:'GTA5',
+    price:98.0,
+    num:2
+},
+{
+    name:'Left4Dead 2',
+    price:7.0,
+    num: 4
+
+},
+{
+    name:'荒野大镖客',
+    price:249.0,
+    num:1
+}
+]);
